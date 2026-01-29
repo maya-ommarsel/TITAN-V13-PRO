98522a6e1e3cf0ad00910f419456f91fd0cdc786
maya-ommarsel <aimusicmaya@gmail.com>
2026-01-29 22:43:11 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'titan-v13-ultimate';
+const CACHE_NAME = 'titan-v13-supreme-cache';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
