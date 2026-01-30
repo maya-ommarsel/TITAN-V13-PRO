e97b01fc2fa95127a128268f50c7e7db43863f79
maya-ommarsel <aimusicmaya@gmail.com>
2026-01-31 06:55:11 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js');
 }
+const CACHE_NAME = 'titan-v13-secure-cache';
+const assets = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
