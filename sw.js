b6aed694484866440484ba5538f5be3d08719328
maya-ommarsel <aimusicmaya@gmail.com>
2026-01-31 10:35:45 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'titan-supreme-v13-cache';
-const urlsToCache = ['/', '/index.html', '/manifest.json'];
-self.addEventListener('install', event => {
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+const CACHE_NAME = 'titan-supreme-v13';
+const assets = ['/', '/index.html', '/manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
 });
-self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
